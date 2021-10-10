4161a9f4f5c4f1de79e9fa1d397acd41b5a61367
Augustin DESVALLEES <adesvall@e2r1p13.clusters.42paris.fr>
2021-10-10 20:02:58 +0200

    lfbtbr

--- minishell/srcs/main_parsing.c
+++ minishell/srcs/main_parsing.c
@@ -6,16 +6,61 @@
 /*   By: adesvall <adesvall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 14:53:32 by adesvall          #+#    #+#             */
-/*   Updated: 2021/10/09 16:42:11 by adesvall         ###   ########.fr       */
+/*   Updated: 2021/10/10 19:01:46 by adesvall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int ft_parsing(char *line)
+typedef union u_dir
 {
-    
+    char *file;
+    int pipe;
+}               t_dir;
 
+typedef struct s_proc
+{
+    char    *command;
+    t_dir   dir_in;
+    t_dir   dir_out;
+    
+}               t_proc;
+
+void    skip_quotes(char *line, int *i)
+{
+    char c;
+
+    if (line[*i] != '\'' && line[*i] != '"')
+        return ;
+    c = line[*i];
+    *i++;
+    while (line[*i] != c)
+        *i++;
+    *i++;
+}
+
+char **split_process(char *line)
+{
+    char **res;
+    int i;
+
+    i = 0;
+    while (line[i])
+    {
+        skip_quotes(line, &i);
+        if (line[i] == '|')
+            line[i] = '\n';
+        i++;
+    }
+    return (ft_split(line, '\n'));
+}
+
+int parse_line(char *line)
+{
+    char *processes;
+
+    processes = split_process(line);
+    
 
     return (0);
 }
