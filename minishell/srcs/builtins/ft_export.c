91bfa778cbc8a05485343c49de8576000a7b7c7d
Ulysse PEYRET <upeyret@e3r13p13.clusters.42paris.fr>
2022-01-07 14:29:13 +0100

    parsing export OK

--- minishell/srcs/builtins/ft_export.c
+++ minishell/srcs/builtins/ft_export.c
@@ -77,46 +77,60 @@ int	export_sort(t_var *env)
 	return (0);
 }
 
+
 int	ft_export(char **argv, t_var **env)
 {
 	int		i;
 	char	*egal;
+	int		flag;
+	int		out;
 
 	i = 1;
+	out = 0;
 	if (!argv[1])
 		return (export_sort(*env));
 	while (argv[i])
 	{
+		flag = 0;
+		if (ft_strlen(argv[i]) == 0)
+		{
+			printf("export: `': not a valid identifier\n");
+			flag = 1;		
+		}
 		egal = ft_strchr(argv[i], '=');
 		if (!egal)
 		{
 			i++;
+			// printf("OUT\n");
 			continue ;
 		}
 		*egal = 0; // Message d'erreur de export $TOTO=12 incomplet quand TOTO=11 par exemple
-		printf("Egal + 1 : %s\n", (egal + 1));
-		printf("Len : %zu\n",ft_strlen(egal + 1));
-		printf("Argv[i + 1] : %s\n", argv[i + 1]);
-		printf("Argv[i] : %s\n", argv[i]);	
-		printf("Len argv[i]: %zu\n",ft_strlen(argv[i]));
+		// printf("Egal + 1 : %s\n", (egal + 1));
+		// printf("Len : %zu\n",ft_strlen(egal + 1));
+		// printf("Argv[i + 1] : %s\n", argv[i + 1]);
+		// printf("Argv[i] : %s\n", argv[i]);	
+		// printf("Len argv[i]: %zu\n",ft_strlen(argv[i]));
 		if (ft_strlen(argv[i]) == 0)
 		{
 			printf("export: `=%s': not a valid identifier\n", egal + 1);
-			return (1);			
+			flag = 1;		
 		}
-		if (*(egal + 1) == 0 && argv[i + 1] != 0)
+		if (*(egal + 1) == 0 && argv[i + 1] != 0 && flag == 0)
 		{
 			printf("export: `%s': not a valid identifier\n", argv[i + 1]);
-			return (1);
+			flag = 1;
 		}
-		if (!check_var_name(argv[i]))
+		if (!check_var_name(argv[i]) && flag == 0)
 		{
 			printf("export: `%s': not a valid identifier\n", argv[i]);
-			return (1);
+			flag = 1;
 		}
-		add_var(env, argv[i], egal + 1);
+		if (flag == 0)
+			add_var(env, argv[i], egal + 1);
 		*egal = '=';
+		out = out + flag;
 		i++;
 	}
-	return (0);
+	out = (out > 0) * 1;
+	return (out);
 }
